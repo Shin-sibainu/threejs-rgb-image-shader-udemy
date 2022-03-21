199fd9f6d80ac3283eec334ea4e7eda25e1195d3
Shin-sibainu <shincode0712@gmail.com>
2022-03-21 21:32:09 +0900

    createmesh

--- src/index.js
+++ src/index.js
@@ -25,9 +25,86 @@ function smoothScroll() {
   requestAnimationFrame(smoothScroll);
 }
 
-// class EffectCanvas {
-//   constructor()
-// }
+class EffectCanvas {
+  constructor() {
+    this.container = document.querySelector("main");
+    this.images = [...document.querySelectorAll("img")];
+    this.meshItems = [];
+    this.setupCamera();
+    this.createMeshItems();
+    this.render();
+  }
+
+  get viewport() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let aspectRatio = width / height;
+    return {
+      width,
+      height,
+      aspectRatio,
+    };
+  }
+
+  setupCamera() {
+    window.addEventListener("resize", this.onWindowResize.bind(this));
+    this.scene = new THREE.Scene();
+
+    //perspectivecamera
+    let perspective = 1000;
+    const fov =
+      (180 * (2 * Math.atan(window.innerHeight / 2 / perspective))) / Math.PI;
+    this.camera = new THREE.PerspectiveCamera(
+      fov,
+      this.viewport.aspectRatio,
+      1,
+      1000
+    );
+    this.camera.position.set(0, 0, perspective);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.container.appendChild(this.renderer.domElement);
+  }
+
+  onWindowResize() {
+    init();
+    this.camera.aspect = this.viewport.aspectRatio;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.viewport.width, this.viewport.height);
+  }
+
+  createMeshItems() {}
+}
+
+class MeshItems {
+  constructor(element, scene) {
+    this.element = element;
+    this.scene = scene;
+    this.offset = new THREE.Vector2(0, 0);
+    this.sizes = new THREE.Vector2(0, 0);
+    this.createMesh();
+  }
+
+  getDimensions() {
+    const { width, height, top, left } = this.element.getBoundingClientRect();
+    this.sizes.set(width, height);
+    this.offset.set(
+      left - window.innerWidth / 2 + width / 2,
+      (-top * window.innerHeight) / 2 - height / 2
+    );
+  }
+
+  createMesh() {
+    this.geometry = new THREE.PlaneGeometry(1, 1, 30, 30);
+    this.imageTexture = new THREE.TextureLoader().load(this.element.src);
+    this.uniforms = {};
+    this.material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+    });
+  }
+}
 
 init();
 
